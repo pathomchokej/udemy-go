7699d09281865215fd938c6555899c1f7c9664d0
Pathomchoke J <tummeng.j@gmail.com>
2022-02-07 13:50:53 +0700

    try on another package

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello")
+	//init
+	fmt.Println("ENVIROMENT")
+	fmt.Println("ENV: ", os.Getenv("ENV"))
+	fmt.Println("TASK_TYPE: ", os.Getenv("TASK_TYPE"))
+	fmt.Println("API_VERSION: ", os.Getenv("API_VERSION"))
+	fmt.Println("CONFIG_FILE: ", os.Getenv("CONFIG_FILE"))
+	fmt.Println("BINARY_NAME: ", os.Getenv("BINARY_NAME"))
+	fmt.Println("PORT: ", os.Getenv("PORT"))
+	fmt.Println("AWS_REGION: ", os.Getenv("AWS_REGION"))
+	fmt.Println("AWS_ACCOUNT_ID: ", os.Getenv("AWS_ACCOUNT_ID"))
+	fmt.Println("AWS_PROFILE: ", os.Getenv("AWS_PROFILE"))
+	fmt.Println("DESIRED_COUNT: ", os.Getenv("DESIRED_COUNT"))
+	fmt.Println("")
 }
